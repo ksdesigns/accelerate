2c20ccfed70e4849e8f9a1077366967b22372781
KS Designs <kayseniordesigns@gmail.com>
2017-11-09 17:15:40 -0500

    completed icon-only footer

--- wp-content/themes/accelerate-theme/footer.php
+++ wp-content/themes/accelerate-theme/footer.php
@@ -10,7 +10,6 @@
  */
 ?>
 
-		</div><!-- #main -->
 
 		<footer id="colophon" class="site-footer clearfix" role="contentinfo">
 			<div class="site-info">
@@ -20,13 +19,18 @@
 				</div>
 
 				<nav class="social-media-navigation" role="navigation">
-					<?php wp_nav_menu( array( 'theme_location' => 'social-media', 'menu_class' => 'social-media-menu' ) ); ?>
+					<?php wp_nav_menu( 
+                        array( 
+                            'theme_location' => 'social-media', 
+                            'menu_class' => 'social-media-menu',
+                            'link_before' => '<span class=""scree-reader-text>',
+                            'link_after' => '</span>'
+                        ) 
+                    ); ?>
 				</nav>
 			</div><!-- .site-info -->
 		</footer><!-- #colophon -->
 
-	</div><!-- #page -->
 
 	<?php wp_footer(); ?>
-</body>
-</html>
+
